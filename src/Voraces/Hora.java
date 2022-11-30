02556680909a5fa9fb59072fc2ba63f1d9712710
Alberto-Saiz-Serrano <alberto.saiz@hotmail.es>
2022-11-30 12:16:29 +0100

    Voraces

---
+++ src/Voraces/Hora.java
@@ -0,0 +1,13 @@
+package Voraces;
+
+public class Hora {
+    private int hora;
+    private int minutos;
+    public Hora(int hora, int minutos){
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+    public int transformaAMinutos(){
+        return ((this.hora*60)+(this.minutos));
+    }
+}
---
+++ src/Voraces/Moneda.java
@@ -0,0 +1,25 @@
+package Voraces;
+
+public class Moneda {
+    private int valor;
+    private int cantidad;
+
+    public Moneda(int valor, int cantidad){
+        this.valor = valor;
+        this.cantidad = cantidad;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
